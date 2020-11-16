0fe8aa178747e82122e3dab4e0aa121ac549d9b2
miracle <soberlyxq@vip.qq.com>
2020-11-16 15:28:55 +0800

    Update styleguide.config.js

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,5 @@ module.exports = {
   webpackConfig: require("./config/webpack.config.js"),
   components: "src/components/*/index.tsx",
   usageMode: 'collapse',
-  title: "云空间通用组件",
+  title: "通用组件",
 };
